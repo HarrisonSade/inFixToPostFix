d554e29d7198ea1bbd27a4f2703577e7343ef250
boomer199 <ec.villiger@gmail.com>
2022-10-31 21:51:49 -0700

    probably finished

---
+++ src/infixtopostfixprogram/InfixToPostFixProgram.java
@@ -0,0 +1,43 @@
+package infixtopostfixprogram;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author everett v
+ */
+public class InfixToPostFixProgram {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String input;
+
+        while (true) {
+            System.out.print("Enter infix: ");
+            input = getString(); // read a string from keyboard
+            if (input.equals("")) {
+                break;
+            }
+            InFix t = new InFix(input);
+            System.out.println(t.infixToPostfix(input));
+            break;
+        } 
+        
+    } 
+    
+//--------------------------------------------------------------
+
+    public static String getString(){
+        try (Scanner scanner = new Scanner(System.in)) {
+            String s = scanner.nextLine(); 
+            return s;
+        } 
+    }
+}
+
+
+////////////////////////////////////////////////////////////////
+
+
+
